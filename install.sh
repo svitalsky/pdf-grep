3e5c568d9636b5ff6b128a25bb36011801b15730
Marcel Svitalský <marcel.svitalsky@gmail.com>
2021-02-15 01:57:16 +0100

    Improved install script

--- install.sh
+++ install.sh
@@ -1,8 +1,30 @@
 #!/bin/sh
 
+errorExit() {
+    [ "$1" != "" ] && \
+        errMsg="$1" || \
+        errMsg="Unknown error"
+    echo "$errMsg. Exiting now!" >&2
+    exit 1
+}
+
 cd "$( dirname "$0" )"
 
-[ -d "$HOME/bin" ] && \
-    [ -f pdf-grep.py ] && \
-    /bin/cp -v -f pdf-grep.py "$HOME/bin/pdf-grep" && \
-    chmod 755 -v "$HOME/bin/pdf-grep"
+[ $( id -u ) = 0 ] && \
+    INST_DIR=/usr/local/bin || \
+    INST_DIR=$HOME/bin
+
+if ! [ -d "$INST_DIR" ] ; then
+    mkdir "$INST_DIR" || errorExit "Failed to create installation directory '$INST_DIR'"
+    echo "$PATH" | grep -q "$INST_DIR" || \
+        echo "Please add the installation directory '$INST_DIR' to path!"
+fi
+
+if [ -f pdf-grep.py ] ; then
+    /bin/cp -v -f pdf-grep.py "$INST_DIR/pdf-grep" && \
+    chmod 755 -v "$INST_DIR/pdf-grep"
+else
+    errorExit "Cannot find the script 'pdf=grep.py' to install it. Please check or update your repository"
+fi
+
+exit 0
